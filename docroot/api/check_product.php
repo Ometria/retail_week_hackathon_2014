e1950ad1912a81c8c044bffd3801c5c27f7fc40a
Al James <al@ometria.com>
2014-09-17 22:14:23 +0100

    Fixes

--- docroot/api/check_product.php
+++ docroot/api/check_product.php
@@ -8,4 +8,7 @@ $retailer = $_GET['retailer'];
 $is_listed = product_is_wishlisted($retailer, $pid);
 
 $ret = array('listed'=>$is_listed);
-send_json($ret);
+//send_json($ret);
+
+header('Content-Type: text/javascript');
+echo 'stash_status('.json_encode($ret).');';
