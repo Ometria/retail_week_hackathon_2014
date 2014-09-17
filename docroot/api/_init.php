27d28506ff89cb49acd8d55ce9522800882afc6e
Al James <al@ometria.com>
2014-09-17 21:53:34 +0100

    Fixes

--- docroot/api/_init.php
+++ docroot/api/_init.php
@@ -6,7 +6,7 @@ if (@$_SERVER['HTTP_REFERER']) {
     $origin = $parts[0].'//'.$parts[2];
 }
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: '.$origin);
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Credentials: true');
 
