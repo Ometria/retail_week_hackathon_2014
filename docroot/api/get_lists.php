fec276ffc722520a19b8ee0461eb65cb07e29952
Al James <al@ometria.com>
2014-09-18 10:09:43 +0100

    Fix shit

--- docroot/api/get_lists.php
+++ docroot/api/get_lists.php
@@ -6,5 +6,14 @@ $extended = @$_GET['extended']=='true';
 
 $lists = lists_get_for_user($extended);
 
+$deflist = array(
+        'id'=>'def_'.current_user(),
+        'title'=>'All stashes',
+        'n_users'=>1,
+        'date' => '13th September 2014'
+        );
+
+array_unshift($lists, $deflist);
+
 $ret = array('lists'=>$lists);
 send_json($ret);
